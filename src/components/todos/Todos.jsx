408eaa50f6d34b72727c44b3f44c92b4d0e38081
Said Sadykzhanov <saidsadykzhanov1@gmail.com>
2026-07-21 01:27:27 +0600

    fix bug

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -1,9 +1,16 @@
-import React from "react"
+import React, {useEffect} from "react"
 import "./Todos.css"
 
 function Todos({isMenu, setIsMenu}) {
+    useEffect(() => {
+        if (!isMenu) {
+            setTimeout(() => {
+                document.querySelector(".todos").classList.toggle("over")
+            }, 500)
+        }
+    }, [isMenu])
     return (
-        <div onClick={() => setIsMenu(!isMenu)} className={`todos ${isMenu ? "" : "active"}`}></div>
+        <div onClick={() => setIsMenu(!isMenu)} className={`todos ${isMenu ? "" : `active`}`}></div>
     );
 }
 
