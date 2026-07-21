5471e5a5afbd1b7832b3ba63e8e565fa5ca2495b
Said Sadykzhanov <saidsadykzhanov1@gmail.com>
2026-07-21 19:39:38 +0600

    add folders component layout

--- src/components/folders/Folders.jsx
+++ src/components/folders/Folders.jsx
@@ -2,12 +2,40 @@ import React from "react"
 import "./Folders.css"
 
 import Add from "../../assets/add.svg?react"
+import All from "../../assets/all.svg?react"
+import Delete from "../../assets/delete.svg?react"
 
 function Folders({isMenu, setIsMenu}) {
     return (
         <div className={`folders ${isMenu ? "active" : ""}`}>
+            <button className="folders__all">
+                <All className="folders__all-icon"/>
+                Все задачи
+            </button>
+
+            <div className="folders__list">
+                <button className="folders__folder">
+                    <div className="folders__folder-color"></div>
+                    <p className="folders__folder-title">Покупки</p>
+                    <Delete className="folders__folder-delete"/>
+                </button>
+
+                <button className="folders__folder">
+                    <div className="folders__folder-color"></div>
+                    <p className="folders__folder-title">Фронтенд</p>
+                    <Delete className="folders__folder-delete"/>
+                </button>
+
+                <button className="folders__folder">
+                    <div className="folders__folder-color"></div>
+                    <p className="folders__folder-title">Фильмы и сериал</p>
+                    <Delete className="folders__folder-delete"/>
+                </button>
+
+            </div>
+
             <button className="folders__add">
-                <Add className="folders__all-icon"/>
+                <Add className="folders__add-icon"/>
                 Добавить папку
             </button>
         </div>
