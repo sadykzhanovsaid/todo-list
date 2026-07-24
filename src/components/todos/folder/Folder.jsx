c64ca02f538503f113d03f2cf39a8a94c8882634
Said Sadykzhanov <saidsadykzhanov1@gmail.com>
2026-07-24 17:38:57 +0600

    partially implement todos component

--- src/components/todos/folder/Folder.jsx
+++ src/components/todos/folder/Folder.jsx
@@ -1,7 +1,9 @@
-import React, {useState} from "react"
+import React, {useState, useRef} from "react"
 import "./Folder.css"
 
-function getTextWidth(text, font = "32px Inter") {
+import Change from "../../../assets/change.svg?react"
+
+function getTextWidth(text, font = "32px Montserrat") {
     const canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"))
     const context = canvas.getContext("2d")
     context.font = font
@@ -11,28 +13,46 @@ function getTextWidth(text, font = "32px Inter") {
 }
 
 function Folder({folder, updateFolder}) {
-    const fontSpec = "32px Inter"
+    const fontSpec = "600 32px Montserrat"
     const textWidth = getTextWidth(folder.title || "", fontSpec)
     const inputWidth = Math.max(textWidth + 16, 40)
     const [isFocus, setIsFocus] = useState(true)
+    const inputRef = useRef(null)
+
+    function handleChangeClick(e) {
+        e.stopPropagation()
+
+        if (inputRef.current) {
+            inputRef.current.focus()
+        }
+    }
 
     return (
         <div className="folder">
-            <input
-                type="text"
-                style={{
-                    width: `${inputWidth}px`,
-                    color: folder.color,
-                    caretColor: folder.color
-                }}
-                value={folder.title}
-                className={`folder__title ${isFocus ? "truncated" : ""}`}
-                onChange={(e) => updateFolder(folder.id, e.target.value)}
-                onClick={(e) => e.stopPropagation()}
-                onFocus={() => setIsFocus(false)}
-                onBlur={() => setIsFocus(true)}
-            />
-            {folder.id} {folder.title} {folder.color}
+            <div className="folder__title-block">
+                <input
+                    type="text"
+                    style={{
+                        width: `${inputWidth}px`,
+                        color: folder.color,
+                        caretColor: folder.color
+                    }}
+                    ref={inputRef}
+                    value={folder.title}
+                    className={`folder__title ${isFocus ? "truncated" : ""}`}
+                    onChange={(e) => updateFolder(folder.id, e.target.value)}
+                    onClick={(e) => e.stopPropagation()}
+                    onFocus={() => setIsFocus(false)}
+                    onBlur={() => setIsFocus(true)}
+                />
+
+                <div
+                    className="folder__change"
+                    onClick={(e) => handleChangeClick(e)}
+                >
+                    <Change/>
+                </div>
+            </div>
             <div className="folder__line"></div>
         </div>
     );
